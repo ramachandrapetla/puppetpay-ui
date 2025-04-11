be153ab412638ff66a46f3e739e9fc015e6065d1
ramachandrapetla <ramachandra1996@gmail.com>
2025-04-11 17:11:51 -0500

    added wave background

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import HeroSection from './components/Landing/HeroSection'
 import { NavBar } from './components/NavBar/NavBar'
-import { BlobBackground } from './components/BlobBackground'
-import { Section } from './layouts/Section'
 import { Features } from './components/Features/Features'
+import { BackgroundWave } from './components/HeroBackground/BackgroundWave'
 
 function App() {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
-      <BlobBackground />
+      <BackgroundWave />
       <NavBar />
       <HeroSection />
       <Features />
